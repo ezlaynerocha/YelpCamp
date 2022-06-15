b8ec602fe6f630c8aa6c3e17801952cb8f209ee9
Ezlayne Rocha <ezlaynerocha@hotmail.com>
2022-06-15 15:35:51 -0300

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/89701610",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur asperiores dicta pariatur veniam in ullam, odit culpa vero facere ducimus perspiciatis, tempore voluptatem obcaecati sequi totam consequatur nostrum? Dolor, excepturi.",
+      price,
     });
     await camp.save();
   }
